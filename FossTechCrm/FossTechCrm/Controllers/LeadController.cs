65db3c80b66643d40b7135889814471c144a42d1
kasif780780 <kasif780780@gmail.com>
2019-07-15 01:25:13 +0530

    finally we applied edit and delete function using ajax jquery datatable

--- FossTechCrm/FossTechCrm/Controllers/LeadController.cs
+++ FossTechCrm/FossTechCrm/Controllers/LeadController.cs
@@ -1,9 +1,11 @@
 ﻿using FossTechCrm.Entities;
 using FossTechCrm.Models;
 using FossTechCrm.Services;
+using FossTechCrm.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,33 +27,76 @@ namespace FossTechCrm.Controllers
 
 
             var leads = db.Leads.ToList();
-                return Json (new { data = leads }, JsonRequestBehavior.AllowGet);
-            
+            return Json(new { data = leads }, JsonRequestBehavior.AllowGet);
+
         }
 
         [HttpGet]
-        public ActionResult Action()
+        public ActionResult Action(int? id)
         {
-            Lead lead = new Lead();
-            return PartialView("_Action", lead);
+            Lead model = new Lead();
+
+            if (id.HasValue) // we trying to edit this record
+            {
+                var leadtype = leadService.GetLeadsbyId(id.Value);
+                model.LeadID = leadtype.LeadID;
+                model.CompanyName = leadtype.CompanyName;
+                model.Description = leadtype.Description;
+                model.Email = leadtype.Email;
+                model.FirstName = leadtype.FirstName;
+                model.LeadSource = leadtype.LeadSource;
+                model.Phone = leadtype.Phone;
+                model.Ratings = leadtype.Ratings;
+                model.Status = leadtype.Status;
+            }
+
+            //else // we trying create record
+            //{
+
+            //}
+
+            return PartialView("_Action", model);
         }
 
         [HttpPost]
         public JsonResult Action(Lead model)
         {
-            JsonResult json = new JsonResult();
-            Lead lead = new Lead();
-            lead.CompanyName = model.CompanyName;
-            lead.Description = model.Description;
-            lead.Email = model.Email;
-            lead.FirstName = model.FirstName;
-            lead.LeadSource = model.LeadSource;
-            lead.Phone = model.Phone;
-            lead.Ratings = model.Ratings;
-            lead.Status = model.Status;
 
-            var result = leadService.SaveLead(lead);
-            if(result)
+
+            JsonResult json = new JsonResult();
+            var result = false;
+            if (model.LeadID > 0)
+            {
+                var lead = leadService.GetLeadsbyId(model.LeadID);
+                lead.CompanyName = model.CompanyName;
+                lead.Description = model.Description;
+                lead.Email = model.Email;
+                lead.FirstName = model.FirstName;
+                lead.LeadSource = model.LeadSource;
+                lead.Phone = model.Phone;
+                lead.Ratings = model.Ratings;
+                lead.Status = model.Status;
+
+                result = leadService.UpdateLead(lead);
+            }
+
+            else
+            {
+                Lead lead = new Lead();
+                lead.LeadID = model.LeadID;
+                lead.CompanyName = model.CompanyName;
+                lead.Description = model.Description;
+                lead.Email = model.Email;
+                lead.FirstName = model.FirstName;
+                lead.LeadSource = model.LeadSource;
+                lead.Phone = model.Phone;
+                lead.Ratings = model.Ratings;
+                lead.Status = model.Status;
+
+                result = leadService.SaveLead(lead);
+            }
+
+            if (result)
             {
                 json.Data = new { Success = true };
             }
@@ -62,5 +107,46 @@ namespace FossTechCrm.Controllers
             }
             return json;
         }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Lead model = new Lead();
+
+            var leadtype = leadService.GetLeadsbyId(id);
+            model.LeadID = leadtype.LeadID;
+
+             
+
+            return PartialView("_Delete", model);
+        }
+
+        [HttpPost]
+        public JsonResult Delete(Lead model)
+        {
+
+
+            JsonResult json = new JsonResult();
+            var result = false;
+            var lead = leadService.GetLeadsbyId(model.LeadID);
+
+            result = leadService.DeleteLead(lead);
+
+
+            if (result)
+            {
+                json.Data = new { Success = true };
+            }
+
+            else
+            {
+                json.Data = new { Success = false, Message = "unable to delete Leads" };
+            }
+            return json;
+        }
+
+
+
+
     }
 }
--- FossTechCrm/FossTechCrm/Services/LeadService.cs
+++ FossTechCrm/FossTechCrm/Services/LeadService.cs
@@ -10,17 +10,42 @@ namespace FossTechCrm.Services
     public class LeadService
     {
         
+        //For List
         public IEnumerable<Lead> GetLeads()
         {
             var context = new ApplicationDbContext();
            return context.Leads.ToList();
         }
+        //For Edit
+        public Lead GetLeadsbyId (int id)
+        {
+            var context = new ApplicationDbContext();
+            return context.Leads.Find(id);
+        }
 
+        //For Save
         public bool SaveLead(Lead lead)
         {
             var context = new ApplicationDbContext();
             context.Leads.Add(lead);
             return context.SaveChanges()>0;
+        }
+
+        //Update data
+        public bool UpdateLead(Lead lead)
+        {
+            var context = new ApplicationDbContext();
+            context.Entry(lead).State = System.Data.Entity.EntityState.Modified;
+            return context.SaveChanges() > 0;
+        }
+
+        public bool DeleteLead(Lead lead)
+        {
+            var context = new ApplicationDbContext();
+            context.Entry(lead).State = System.Data.Entity.EntityState.Deleted;
+            
+       
+            return context.SaveChanges() > 0;
         }
     }
 }
